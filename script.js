6cfd4157b7d54c4b26f7b5a0c8b3af1988759778
jeronimo0109 <jeronimogomez0107@gmail.com>
2024-01-18 02:29:45 -0500

    primeros avanzas de la main page

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 'use strict'
+
+function quitLogin() {
+    document.getElementById('login-container').classList.add('disguise')
+    document.getElementById('MainPage-container').classList.add('appear')
+    document.getElementById('MainPage-container').style.display = "block"
+}
+
+function Login() {
+    document.getElementById('login-container').classList.remove('disguise')
+    document.getElementById('login-container').classList.add('appear')
+}
+
